c841e614cdc4ab9b2aac853a90ee7daa11158b38
hjwforever <hjwbelieve@gmail.com>
2021-01-27 23:57:31 +0800

    投篮动效

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Eva Demo
 
 > - 除了背景， 添加了篮球， 篮板， 篮框
+> - 点击投球即可出现 投球动画, 投球完毕后可再次点击动画
 
-- 效果图 `docs/statics/效果图.png`
-  ![效果图](https://s3.ax1x.com/2021/01/27/sz5GNT.png)
+- 效果图 `docs/statics/效果图.gif`
+  ![效果图](https://s3.ax1x.com/2021/01/27/szz54J.gif)
+
+- 界面 `docs/statics/界面.png`
+  ![界面](https://s3.ax1x.com/2021/01/27/szz4N4.png)
 
 ## Develop
 
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ import { Img, ImgSystem } from '@eva/plugin-renderer-img';
 import { EventSystem } from '@eva/plugin-renderer-event';
 import { SpriteAnimationSystem, SpriteAnimation } from '@eva/plugin-renderer-sprite-animation';
 import { RenderSystem } from '@eva/plugin-renderer-render';
-import { TransitionSystem } from '@eva/plugin-transition';
+import { TransitionSystem, Transition } from '@eva/plugin-transition';
 import { GraphicsSystem } from '@eva/plugin-renderer-graphics';
 import { TextSystem } from '@eva/plugin-renderer-text';
-
+import createBtn from './btn';
 
 declare global {
   interface Window {
@@ -21,7 +21,7 @@ declare global {
 resource.addResource(resources);
 
 // 资源预加载
-resource.preload();
+// resource.preload();
 resource.on(LOAD_EVENT.START, e => {
   console.log('start', e);
 }); // 开始loader
@@ -76,12 +76,17 @@ function initBG() {
   game.scene.addChild(bg)
 }
 
-// 添加篮球
-function initBasketBall() {
+interface Position {
+  x: number,
+  y: number
+}
+
+// 添加篮球 及其 投篮的过渡动画
+function initBasketBall(position: Position, targetPosition: Position) {
   const basketball = new GameObject('basketball', {
       size: { width: 79, height: 79 },
       origin: { x: 0.5, y: 0.5 },
-      position: { x: 500, y: 1100 },
+      position,
       anchor: {
         x: 0,
         y: 0,
@@ -92,7 +97,154 @@ function initBasketBall() {
         resource: 'basketball',
       })
   );
+  const animation = basketball.addComponent(new Transition());
+  animation.group = {
+    idle: [
+      {
+        name: 'scale.x',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: 1,
+            tween: 'ease-out',
+          },
+          {
+            time: 300,
+            value: 1.2,
+            tween: 'ease-in',
+          },
+          {
+            time: 600,
+            value: 1,
+          },
+        ],
+      },
+      {
+        name: 'scale.y',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: 1,
+            tween: 'ease-out',
+          },
+          {
+            time: 300,
+            value: 1.2,
+            tween: 'ease-in',
+          },
+          {
+            time: 600,
+            value: 1,
+          },
+        ],
+      },
+    ],
+    move: [
+      {
+        name: 'position.x',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: position.x,
+            tween: 'ease-in',
+          },
+          {
+            time: 300,
+            value: targetPosition.x,
+            tween: 'ease-out',
+          },
+        ],
+      },
+      {
+        name: 'position.y',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: position.y,
+            tween: 'ease-out',
+          },
+          {
+            time: 300,
+            value: targetPosition.y,
+          },
+        ],
+      },
+    ],
+    fall: [
+      {
+        name: 'position.x',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: targetPosition.x,
+            tween: 'ease-in',
+          },
+          {
+            time: 300,
+            value: targetPosition.x+1,
+            tween: 'ease-out',
+          },
+        ],
+      },
+      {
+        name: 'position.y',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: targetPosition.y,
+            tween: 'ease-in',
+          },
+          {
+            time: 1200,
+            value: position.y,
+            tween: 'ease-in',
+          },
+        ],
+      },
+    ],
+    reset: [
+      {
+        name: 'position.x',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: targetPosition.x,
+            tween: 'ease-in',
+          },
+          {
+            time: 300,
+            value: position.x,
+            tween: 'ease-out',
+          },
+        ],
+      },
+      {
+        name: 'position.y',
+        component: basketball.transform,
+        values: [
+          {
+            time: 0,
+            value: position.y,
+            tween: 'ease-out',
+          },
+          {
+            time: 300,
+            value: position.y+1,
+          },
+        ],
+      },
+    ]
+  };
   game.scene.addChild(basketball)
+
+  return {basketball, animation};
 }
 
 // 添加篮板
@@ -144,17 +296,14 @@ function initBasketBack() {
   game.scene.addChild(basketBack)
 }
 
-// 添加篮网
-function initBasketFront() {
+// 添加篮网 及其 投篮时对应的帧动画
+function initBasketFront(position: Position) {
   const basetFront = new GameObject('board', {
     size: {
       width: 166,
       height: 157,
     },
-    position: {
-      x: 120,
-      y: 450,
-    },
+    position,
     // position: {
     //   x: 80,
     //   y: -760,
@@ -172,24 +321,81 @@ function initBasketFront() {
     })
   );
 
-  const playAnim = () => {
-    anim.resource = 'boardGoal';
-    setTimeout(() => {
-      anim.resource = 'boardIdle';
-    }, 900);
+  const playAnim = (status: number) => {
+    switch(status) {
+      case 0: {
+        anim.resource = 'boardIdle';
+        break;
+      }
+      case 1: {
+        anim.resource = 'boardGoal';
+        setTimeout(() => {
+          anim.resource = 'boardIdle';
+        }, 900);
+        break;
+      }
+      default:
+        anim.resource = 'boardIdle';
+        break;
+    }
   };
 
   game.scene.addChild(basetFront);
   
   window.playAnim = playAnim;
+
+  return { basetFront, playAnim };
 }
 
 function initAllResource() {
-  initBG();
-  initBasketBall();
-  initBackboard();
-  initBasketBack();
-  initBasketFront();
+  const position: Position = { x: 500, y: 1100 }; // 篮球初始位置
+  const pos: Position = {x: 120,y: 450 }; // 篮筐位置
+   
+
+  initBG(); // 背景
+  const {basetFront, playAnim } = initBasketFront(pos);  // 篮筐(篮网下部分) 及其动画
+  pos.x += basetFront.transform.size.width / 2; // 投篮时篮球目标位置
+  const { animation } = initBasketBall(position, pos); // 篮球 及其 投篮的过渡动画
+  initBackboard(); // 篮板
+  initBasketBack(); // 篮网上半部分
+
+
+  const btn = createBtn({
+  text: '投球',
+  transform: {
+    position: {
+      x: 0,
+      y: -400,
+    },
+    origin: {
+      x: 0.5,
+      y: 0.5,
+    },
+    anchor: {
+      x: 0.5,
+      y: 1,
+    },
+  },
+  callback: () => {
+    animation.play('idle', 2);
+    let status = 0;
+    animation.on('finish', name => {
+      // console.log(name);
+      if (status ===0 && name === 'idle') {
+        animation.play('move', 1);  // 投球
+        status = 1;
+      } else if(status ===1 && name === 'move') {
+        playAnim(1); // 篮筐动画
+        animation.play('fall', 1); // 球落下
+        status = 2;
+      } else if (status ===2 && name === 'fall') {
+       animation.play('reset', 1);
+       animation.play('idle', 2);
+      }
+    });
+  },
+});
+  game.scene.addChild(btn);
 }
 
 initAllResource();
